e8e912b69a47284ce18cef09150edaa935cba0b5
gjdbg148525 <110419265+gjdbg148525@users.noreply.github.com>
2022-09-02 13:34:08 +0800

    Delete main directory

--- main/java/com/tighug/difficulty/entity/ILivingEntity.java
+++
@@ -1,8 +0,0 @@
-package com.tighug.difficulty.entity;
-
-import net.minecraft.util.DamageSource;
-
-public interface ILivingEntity extends IEntity{
-
-    boolean publicCheckTotemDeathProtection(DamageSource damageSource);
-}
